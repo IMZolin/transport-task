52b49c68181b90381d57e41a851cbf1e42810fb1
IMZolin <zolin5269@gmail.com>
2023-03-08 17:40:36 +0300

    Update class 2

--- src/transport_problem.py
+++ src/transport_problem.py
@@ -4,20 +4,20 @@ import numpy as np
 class TransportProblem:
     def __init__(self):
         self.weight_matrix = np.array([])  # матрица c_i_j
-        self._basis_solution_matrix = np.array([])  # матрица, в базисных переменных которой стоят числа
-        self._new_basis_variable = np.array([])  # координата новой вводимой переменной
-        self._loop_recalculation = np.array([])  # массив координат точек цикла, установленных в определённом порядке
-        self._export_a = np.array([])  # a, количество груза в пунктах хранения(a_i в i-ом пункте)
-        self._import_b = np.array([])  # b, количество груза в пунктах назначения(b_j в j-ом пункте)
-        self._potential_u = np.array([])
-        self._potential_v = np.array([])
-        self._is_optimal = False
-        self._is_closed = True
+        self.basis_solution_matrix = np.array([])  # матрица, в базисных переменных которой стоят числа
+        self.new_basis_variable = np.array([])  # координата новой вводимой переменной
+        self.loop_recalculation = np.array([])  # массив координат точек цикла, установленных в определённом порядке
+        self.export_a = np.array([])  # a, количество груза в пунктах хранения(a_i в i-ом пункте)
+        self.import_b = np.array([])  # b, количество груза в пунктах назначения(b_j в j-ом пункте)
+        self.potential_u = np.array([])
+        self.potential_v = np.array([])
+        self.is_optimal = False
+        self.is_closed = True
 
     def __repr__(self):
         return f"TRANSPORT PROBLEM:\nМатрица c_i_j:\n{self.weight_matrix}\nМатрица, в числах которых стоят базисные " \
-               f"переменные: \n{self._basis_solution_matrix}\nКоордината новой вводимой переменной: " \
-               f"{self._new_basis_variable}\nМассив координат точек цикла, установленных в определённом порядке: " \
-               f"{self._loop_recalculation}\nКоличество груза в пунктах хранения: {self._export_a}\nПотенциал u: " \
-               f"{self._potential_u}\nПотенциал v: {self._potential_v}\nОптимальность решения: {self._is_optimal}" \
-               f"\nЗадача закрыта: {self._is_closed}"
+               f"переменные: \n{self.basis_solution_matrix}\nКоордината новой вводимой переменной: " \
+               f"{self.new_basis_variable}\nМассив координат точек цикла, установленных в определённом порядке: " \
+               f"{self.loop_recalculation}\nКоличество груза в пунктах хранения: {self.export_a}\nПотенциал u: " \
+               f"{self.potential_u}\nПотенциал v: {self.potential_v}\nОптимальность решения: {self.is_optimal}" \
+               f"\nЗадача закрыта: {self.is_closed}"
